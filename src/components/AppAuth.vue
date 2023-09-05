4838d87adb2d4e06a6c2ea8b96547b1b0eefbc39
Mohammad Mahdi Ghoreishi <mr.mmghoreishi@gmail.com>
2023-09-05 16:54:45 +0330

    Adding default values to the register form

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -75,7 +75,12 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <vee-form v-show="tab === 'register'" :validation-schema="schema" @submit="register">
+          <vee-form
+            v-show="tab === 'register'"
+            :validation-schema="schema"
+            @submit="register"
+            :initial-values="userData"
+          >
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -190,6 +195,9 @@ export default {
         confirm_password: 'required|confirmed:@password',
         country: 'required|excluded:Antarctica',
         tos: 'required'
+      },
+      userData: {
+        country: 'USA'
       }
     }
   },
