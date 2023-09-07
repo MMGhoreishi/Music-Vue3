eb4b0b7521f5fb996e586904f4ac3f05d3175f23
Mohammad Mahdi Ghoreishi <mr.mmghoreishi@gmail.com>
2023-09-07 20:48:50 +0330

    I added some of authentication

---
+++ src/includes/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCiLi7aph3siGjVZy8dNC_Ejva5lPjruh0',
+  authDomain: 'music-ea12b.firebaseapp.com',
+  projectId: 'music-ea12b',
+  storageBucket: 'music-ea12b.appspot.com',
+  appId: '1:850661508365:web:be8944a64216dd5648dc44'
+}
+
+firebase.initializeApp(firebaseConfig)
+
+const auth = firebase.auth()
+const db = firebase.firestore()
+
+export { auth, db }
