f32b4b96fffb28663dfb5e9fde8e9e10e0e5a7af
Mohammad Mahdi Ghoreishi <mr.mmghoreishi@gmail.com>
2023-09-14 21:33:36 +0330

    Adding routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,50 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import AboutView from '@/views/AboutView.vue'
+import ManageView from '@/views/ManageView.vue'
+import useUserStore from '@/stores/user'
+
+const routes = [
+  {
+    name: 'home',
+    path: '/',
+    component: HomeView
+  },
+  {
+    name: 'about',
+    path: '/about',
+    component: AboutView
+  },
+  {
+    name: 'manage',
+    path: '/manage',
+    component: ManageView,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' }
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes,
+  linkExactActiveClass: 'text-yellow-500'
+})
+
+router.beforeEach((to, from, next) => {
+  if (!to.meta.requiresAuth) {
+    next()
+    return
+  }
+
+  const store = useUserStore()
+
+  if (store.userLoggedIn) next()
+  else next({ name: 'home' })
 })
 
 export default router
