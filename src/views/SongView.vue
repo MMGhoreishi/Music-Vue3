407c0a541da62c80c32a1ff08e456bc6a72027af
Mohammad Mahdi Ghoreishi <mr.mmghoreishi@gmail.com>
2023-09-26 23:05:53 +0330

    Updating the Comment Count

--- src/views/SongView.vue
+++ src/views/SongView.vue
@@ -25,7 +25,7 @@
     <div class="bg-white rounded border border-gray-200 relative flex flex-col">
       <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
         <!-- Comment Count -->
-        <span class="card-title">Comments (15)</span>
+        <span class="card-title">Comments ({{ song.comment_count }})</span>
         <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
       </div>
       <div class="p-6">
@@ -125,6 +125,10 @@ export default {
       return
     }
 
+    const { sort } = this.$route.query
+
+    this.sort = sort === '1' || sort === '2' ? sort : '1'
+
     this.song = docSnapshot.data()
     this.getComments()
   },
@@ -150,6 +154,11 @@ export default {
         })
 
         await commentsCollection.add(comment)
+
+        this.song.comment_count += 1
+        await songsCollection.doc(this.$route.params.id).update({
+          comment_count: this.song.comment_count
+        })
       } catch (error) {
         this.comments.pop()
 
@@ -177,6 +186,17 @@ export default {
           ...doc.data()
         })
       ])
+    }
+  },
+  watch: {
+    sort(newVal) {
+      if (newVal === this.$route.query.sort) return
+
+      this.$router.push({
+        query: {
+          sort: newVal
+        }
+      })
     }
   }
 }
