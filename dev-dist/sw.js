5ea19e4d52058231df9b5474515b1ea51e691bb1
Mohammad Mahdi Ghoreishi <mr.mmghoreishi@gmail.com>
2023-10-06 21:22:11 +0330

    Dynamic Route Imports

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7qafmb2rvs"
+    "revision": "0.bahsp777rmo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
