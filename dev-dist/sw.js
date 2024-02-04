9fb04499f94dd38ad5ae7c547c82f75a156b7b24
MMGhoreishi <mr.mmghoreishi@gmail.com>
2024-02-04 14:48:52 +0330

    I imporved pagination style

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.86942f9dbbo"
+    "revision": "0.8od1r06a4og"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
